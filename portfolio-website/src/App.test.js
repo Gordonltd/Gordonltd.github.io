1ce675c7e70a46f30e436b32b9b220ea7cf4df31
Oliver Holden <oliver.holden3@gmail.com>
2020-07-06 17:10:13 +0100

    commit ready for first test build

--- portfolio-website/src/App.test.js
+++ portfolio-website/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Oliver Holden/i);
   expect(linkElement).toBeInTheDocument();
 });
